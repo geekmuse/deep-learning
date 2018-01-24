12d85d2e86b03364f37ca6786bde9e89bfa308aa
Brad Campbell <b@bradleyscampbell.net>
2018-01-23 20:39:46 -0500

    initial

---
+++ first-neural-network/init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#pyenv local anaconda3-4.4.0
+#conda create --name dlnd python=3
+#./setenv.sh
+conda install numpy matplotlib pandas jupyter notebook
+
---
+++ first-neural-network/setenv.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pyenv deactivate anaconda3-4.4.0
+pyenv activate anaconda3-4.4.0/envs/dlnd
